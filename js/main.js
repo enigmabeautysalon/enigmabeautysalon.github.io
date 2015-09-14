0eef944414e79291804293b00f4b736e4942d45b
Bill <castillobil@gmail.com>
2015-09-14 14:00:13 +0400

    Updating Website

--- js/main.js
+++ js/main.js
@@ -22,3 +22,13 @@ $(function() {
         e.preventDefault();
     });
 });
+
+
+//Sort random function
+function random(owlSelector) {
+    owlSelector.children().sort(function(){
+      return Math.round(Math.random()) - 0.5;
+    }).each(function(){
+      $(this).appendTo(owlSelector);
+    });
+}
