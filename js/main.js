403a96740f9bebacfe279598302fecb75c6cf4af
Bill <castillobil@gmail.com>
2015-10-10 15:59:31 +0400

    Replacing new style v2.0 front page

--- js/main.js
+++ js/main.js
@@ -39,3 +39,96 @@ function random(owlSelector) {
     });
 }
 
+// OWL CAROUSEL - SERVICES
+$(document).ready(function() {
+
+// SERVICES TABLE CAROUSEL
+  $(".services").owlCarousel({
+    items: 3,
+    autoPlay : false,
+    stopOnHover : true,
+    lazyLoad : true,
+    navigation : true,
+    navigationText : [
+      "<i class='fa fa-chevron-left'></i>",
+      "<i class='fa fa-chevron-right'></i>"
+      ],
+    itemsDesktop : [1440,3],
+    itemsDesktop : [1228,3],
+    itemsDesktopSmall : [1024,2],
+    itemsTablet: [768,2],
+    beforeInit : function(elem){
+      random(elem);
+    }
+  });
+
+  // BRANDS CAROUSEL
+    $("#brands-slider").owlCarousel({
+      autoPlay : 2000,
+      stopOnHover : false,
+      lazyLoad : true,
+      navigation : false,
+      pagination : false
+    });
+
+    $(".team").owlCarousel({
+      items : 5,
+      autoPlay : false,
+      stopOnHover : false,
+      lazyLoad : true,
+      navigation : true,
+      pagination : false,
+      navigationText : [
+        "<i class='fa fa-chevron-left'></i>",
+        "<i class='fa fa-chevron-right'></i>"
+        ]
+    });
+
+    // About Gallery Images
+    $(function(){
+      $('.chocolat-parent').Chocolat({
+        loop           : true,
+        imageSize     : 'contain',
+        overlayOpacity : 0.6
+      });
+    });
+
+                // Sticky header scroll
+                function scrollnfix() {
+                    window.addEventListener('scroll', function(e){
+                        var distanceY = window.pageYOffset || document.documentElement.scrollTop,
+                        shrinkOn = 20, //header height + body padding - nav height
+                        navi = document.querySelector("#main-menu");
+                        if (distanceY > shrinkOn) {
+                            navi.setAttribute('class', 'sub-menu-bg');
+                            $("#main-menu").css('border-top','transparent');
+                            $("#main-menu").css('border-bottom','transparent');
+                            $("#main-menu").css('box-shadow','0px 2px 2px rgba(0, 0, 0, 0.33)');
+                        } else {
+                            if (navi.className == 'sub-menu-bg'){
+                                navi.setAttribute('class', 'notsticky');
+                                $("#main-menu").css('border-top','2px solid #707070');
+                                $("#main-menu").css('border-bottom','2px solid #707070');
+                                $("#main-menu").css('box-shadow','none');
+                            }
+                        }
+                    });
+                }
+
+                function addLoadEvent(func) {
+                    var oldonload = window.onload;
+                    if (typeof window.onload != 'function') {
+                        window.onload = func;
+                    } else {
+                        window.onload = function() {
+                            if (oldonload) {
+                                oldonload();
+                            }
+                            func();
+                        }
+                    }
+                }
+                addLoadEvent(scrollnfix());
+  });
+            
+            
